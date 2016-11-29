9d8ad3a76eef66bd32efddab26d7567dda31eddf
LeleYami <524379973@qq.com>
2016-11-29 22:28:47 +0800

    Update Mymath.h

--- Mymath.h
+++ Mymath.h
@@ -8,11 +8,31 @@ class Mymath:public TObject{
      Float_t pz;
   public:
      Mymath() { px = py = pz = 0; }
+     
+     void SetX(float x) {
+           px=x;
+           }
+     float GetX(){
+           return px;
+           }
+     
+     void SetY(float y) {
+           py=y; 
+           }
+     float GetY(){
+           return py;
+           }
+  
+     void SetZ(float x,float y,float z) {
+           if(z==x*x+y*y){
+             pz=z;
+             }
+           }
+     float GetZ() {
+           return pz;
+           } 
+  
      void Print() const;
-     void SetX(float x) { px=x; }
-     void SetY(float y) { py=y; }
-     void SetZ(float z) { pz=z; }
-     void SetZ(float x,float y,float z) { z=x*x+y*y; } 
 
 ClassDef(Mymath,1) 
 };
